986a00ba5c5b46208a14ff0838a169004dfe8d53
zhouliangui <zhouliangui@hotmail.com>
2014-11-26 21:32:57 +0800

    number

--- jygj/ui/base/NumberPadTextField.m
+++ jygj/ui/base/NumberPadTextField.m
@@ -14,11 +14,45 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
+        self.keyboardType = UIKeyboardTypeNumberPad;
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(keyboardWillShow:)
+                                                     name:UIKeyboardWillShowNotification
+                                                   object:nil];
     }
     return self;
 }
 
+- (void)keyboardWillShow:(NSNotification *)note {
+    // create custom button
+    UIButton *doneButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    doneButton.frame = CGRectMake(0, 163, 106, 53);
+    doneButton.adjustsImageWhenHighlighted = NO;
+    [doneButton setImage:[UIImage imageNamed:@"DoneUp.png"] forState:UIControlStateNormal];
+    [doneButton setImage:[UIImage imageNamed:@"DoneUp.png"] forState:UIControlStateHighlighted];
+    [doneButton addTarget:self action:@selector(doneButton:) forControlEvents:UIControlEventTouchUpInside];
+    
+    // locate keyboard view
+    UIWindow* tempWindow = [[[UIApplication sharedApplication] windows] objectAtIndex:1];
+    UIView* keyboard;
+    for(int i=0; i<[tempWindow.subviews count]; i++) {
+        keyboard = [tempWindow.subviews objectAtIndex:i];
+        // keyboard view found; add the custom button to it
+        //if([[keyboard description] hasPrefix:@"<UIKeyboard"] == YES)
+        //    [keyboard addSubview:doneButton];
+        
+        if(([[keyboard description] hasPrefix:@"<UIPeripheralHostView"] == YES) || ([[keyboard description] hasPrefix:@"<UIKeyboard"] == YES))
+            [keyboard addSubview:doneButton];
+    }
+}
+
+- (void)doneButton:(id)sender {
+    [self resignFirstResponder];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
